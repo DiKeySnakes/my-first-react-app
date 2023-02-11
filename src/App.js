f16179c031e2d66f5d38d2f2a78baa8af79aa9fa
DiKeySnakes <dikeysnakes@gmail.com>
2023-02-11 11:31:27 +0200

    Add checkbox for completed tasks, add delete task button and clear completed button, remove number property, add ordered list instead

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Overview from './components/Overview';
 import uniqid from 'uniqid';
 
 function App() {
-  const [number, setNumber] = useState(1);
+  const [complete, setComplete] = useState(false);
   const [text, setText] = useState('');
   const [id, setId] = useState(uniqid());
   const [tasks, setTasks] = useState([]);
@@ -15,14 +15,39 @@ function App() {
 
   const onSubmitTask = (e) => {
     e.preventDefault();
-    setNumber(number + 1);
-    setTasks(tasks.concat({ number, text, id }));
+    if (text === '') return;
+    setComplete(false);
+    setTasks(tasks.concat({ complete, text, id }));
     setText('');
     setId(uniqid());
   };
 
+  const completeTask = (id) => {
+    const newTasks = [...tasks];
+    const task = newTasks.find((task) => task.id === id);
+    task.complete = !task.complete;
+    console.log(task);
+    setTasks(newTasks);
+  };
+
+  const deleteTask = (tasks, id) => {
+    const newTasks = tasks.filter((task) => task.id !== id);
+    setTasks(newTasks);
+  };
+
+  const handleClearCompleteTasks = () => {
+    const newTasks = tasks.filter((task) => !task.complete);
+    setTasks(newTasks);
+  };
+
   return (
     <div>
+      <h1>Stuff I need to do</h1>
+      <Overview
+        tasks={tasks}
+        completeTask={completeTask}
+        deleteTask={deleteTask}
+      />
       <form onSubmit={onSubmitTask}>
         <label htmlFor='taskInput'>Enter task</label>
         <input
@@ -33,7 +58,8 @@ function App() {
         />
         <button type='submit'>Add Task</button>
       </form>
-      <Overview tasks={tasks} />
+      <button onClick={handleClearCompleteTasks}>Clear Complete</button>
+      <div>{tasks.filter((task) => !task.complete).length} left to do</div>
     </div>
   );
 }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -2,22 +2,38 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
-function Overview(props) {
-  const { tasks } = props;
+function Overview({ tasks, deleteTask, completeTask }) {
+  const handleClick = (e) => {
+    const id = e.target.parentNode.parentNode.id;
+    console.log(id);
+    deleteTask(tasks, id);
+  };
+
+  const handleCompleteTaskClick = (e) => {
+    const id = e.target.id;
+    console.log(id);
+    completeTask(id);
+  };
 
   return (
-    <ul>
+    <ol>
       {tasks.map((task) => {
         return (
           <li key={task.id}>
-            {task.number}. {task.text}{' '}
-            <button>
+            <input
+              id={task.id}
+              type='checkbox'
+              checked={task.complete}
+              onChange={handleCompleteTaskClick}
+            />{' '}
+            {task.text}{' '}
+            <button id={task.id} onClick={handleClick}>
               <FontAwesomeIcon icon={solid('trash-can')} />
             </button>
           </li>
         );
       })}
-    </ul>
+    </ol>
   );
 }
 
